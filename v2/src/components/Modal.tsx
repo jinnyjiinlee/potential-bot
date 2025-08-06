50541d99ef3dbeb546f4484492f4406748c4c8ce
jinny <ggab.challenge@gmail.com>
2025-08-06 19:30:38 +0900

    fix: fix the supabase path

--- v2/src/components/Modal.tsx
+++ v2/src/components/Modal.tsx
@@ -1,5 +1,5 @@
 import { FormEvent, useState } from 'react';
-import { supabase } from '@/lib/supabaseClient';
+import { supabase } from '../lib/supabaseClient';
 
 type ModalProps = {
   show: boolean;
